a67d9af44d4f58a5e792297051aaa64ee3068be2
onehiro <40028927+onehiro@users.noreply.github.com>
2018-06-15 00:51:09 +0800

    1

--- src/book/action/FileAction.java
+++ src/book/action/FileAction.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
 
 /*@ParentPackage("json-default")
 @Namespace("/")
@@ -15,20 +18,19 @@ import org.apache.struts2.convention.annotation.Result;
 @Scope("prototype")    //多实例方式创建对象
 */
 public class FileAction {
-	private File[] file; // 文件
-	private String[] fileFileName; // 文件名
-	private String[] filePath; // 文件路径
-	private String downloadFilePath; // 文件下载路径
-	private InputStream inputStream;
-
-	/**
-	 * 文件上传
-	 * 
-	 * @return
-	 */
-	@Action(value = "fileUploadAction", results = { @Result(name = "success") })
-	public String fileUpload() {
-		System.out.println("aaaaa");
+	private File[] file;              //文件  
+    private String[] fileFileName;    //文件名   
+    private String[] filePath;        //文件路径
+    private String downloadFilePath;  //文件下载路径
+    private InputStream inputStream; 
+    
+    /**
+     * 文件上传
+     * @return
+     */
+	//@Action(value="fileUploadAction",results={@Result(name="success", type = "json") },params = { "contentType", "text/html" })
+ public String fileUpload() {
+    	System.out.println("aaaaa");
 		String path = ServletActionContext.getServletContext().getRealPath("/attached");
 		File file = new File(path); // 判断文件夹是否存在,如果不存在则创建文件夹
 		if (!file.exists()) {
@@ -45,7 +47,7 @@ public class FileAction {
 					String name = fileName + fileFileName[i].substring(fileFileName[i].lastIndexOf(".")); // 保存在硬盘中的文件名
 					System.out.println("ddddd");
 					FileInputStream inputStream = new FileInputStream(f[i]);
-					FileOutputStream outputStream = new FileOutputStream(path + "\\" + name);
+					FileOutputStream outputStream = new FileOutputStream(path+ "\\" + name);
 					byte[] buf = new byte[1024];
 					int length = 0;
 					while ((length = inputStream.read(buf)) != -1) {
@@ -54,9 +56,9 @@ public class FileAction {
 					System.out.println("eeeee");
 					inputStream.close();
 					outputStream.flush();
-					// 文件保存的完整路径
+					//文件保存的完整路径
 					// 如：D:\tomcat6\webapps\new_infopub\\attached\a0be14a1-f99e-4239-b54c-b37c3083134a.png
-					// filePath[i] = path + "\\" + name;
+					//filePath[i] = path + "\\" + name;
 					filePath[i] = name;
 					System.out.println(name);
 				}
@@ -67,80 +69,67 @@ public class FileAction {
 		}
 		return "success";
 	}
-
+	
 	/**
 	 * @return the file
 	 */
 	public File[] getFile() {
 		return file;
 	}
-
 	/**
-	 * @param file
-	 *            the file to set
+	 * @param file the file to set
 	 */
 	public void setFile(File[] file) {
 		this.file = file;
 	}
-
 	/**
 	 * @return the fileFileName
 	 */
 	public String[] getFileFileName() {
 		return fileFileName;
 	}
-
 	/**
-	 * @param fileFileName
-	 *            the fileFileName to set
+	 * @param fileFileName the fileFileName to set
 	 */
 	public void setFileFileName(String[] fileFileName) {
 		this.fileFileName = fileFileName;
 	}
-
+	
 	/**
 	 * @return the filePath
 	 */
 	public String[] getFilePath() {
 		return filePath;
 	}
-
 	/**
-	 * @param filePath
-	 *            the filePath to set
+	 * @param filePath the filePath to set
 	 */
 	public void setFilePath(String[] filePath) {
 		this.filePath = filePath;
 	}
-
 	/**
 	 * @return the inputStream
 	 */
 	public InputStream getInputStream() {
 		return inputStream;
 	}
-
 	/**
-	 * @param inputStream
-	 *            the inputStream to set
+	 * @param inputStream the inputStream to set
 	 */
 	public void setInputStream(InputStream inputStream) {
 		this.inputStream = inputStream;
 	}
-
 	/**
 	 * @return the downloadFilePath
 	 */
 	public String getDownloadFilePath() {
 		return downloadFilePath;
 	}
-
 	/**
-	 * @param downloadFilePath
-	 *            the downloadFilePath to set
+	 * @param downloadFilePath the downloadFilePath to set
 	 */
 	public void setDownloadFilePath(String downloadFilePath) {
 		this.downloadFilePath = downloadFilePath;
 	}
-
+	
 }
